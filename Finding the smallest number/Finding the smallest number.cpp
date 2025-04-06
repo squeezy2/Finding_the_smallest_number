f9ef7360545494efac5b332a55ac5b478fd3dcd1
awsd <awsd@DESKTOP-EPOMFKP>
2025-04-06 18:55:33 +0300

    Добавьте файлы проекта.

---
+++ Finding the smallest number/Finding the smallest number.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+void fun(float x, float& a, float& b)
+{
+    x = cos(2 * x) + sin(x - 3);
+}
+
+int main()
+{
+    float a, b, x;
+    cout << "a = ? \nb = ? \n";
+    cin >> a, cin >> b;
+    fun(a, b, x);
+    cout << (a >= b ? "b the lowest" : "a is the lowest") << endl;
+    return 0;
+}
